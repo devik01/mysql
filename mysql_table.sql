1dec8505be12bcea8c08859dc6e77f494fcbe280
devik01 <devikrishna0301@gmail.com>
2023-11-02 20:40:25 +0530

    python mysql final code

--- mysql_table.sql
+++ mysql_table.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE employee;
 USE employee;
-CREATE TABLE employee(emp_id int,emp_name varchar(40)NOT NULL,emp_salary int);
-select * from employee;
+CREATE TABLE employeeinfo(emp_id int,emp_name varchar(40)NOT NULL,emp_salary int);
+select * from employeeinfo;
+SET SQL_SAFE_UPDATES = 0;
--- pythonmysql.py
+++ pythonmysql.py
@@ -1,40 +1,29 @@
-'''import pythonmysql
 
-def mysqlconnect():
-
-    # To connect MySQL database 
-    conn = pythonmysql.connect( 
-        host='localhost', 
-        user='root',  
-        password = "root123", 
-        db='employee', 
-        ) 
-    cur = conn.cursor() 
-     # Select query 
-    cur.execute("INSERT INTO employee(emp_id,emp_name,emp_salary) VALUES(100,'Devi Krishna',21000),(101,'Diya Krishna',22000);") 
-    output = cur.fetchall() 
-    for i in output:
-        print(i)
-    # To close the connection 
-    conn.close()
-# Driver Code 
-if __name__ == "__main__" : 
-    mysqlconnect()'''
 
 import mysql.connector
 conn=mysql.connector.connect(host='localhost',user='root',password='root123',db='employee')
 cur=conn.cursor()
 cur.execute("use employee")
-cur.execute("insert into employee values(100,'Devi Krishna',12000);")
-cur.execute("select * from employee")
+#create data (insert query)
+'''cur.execute("insert into employeeinfo values(100,'Devi Krishna',12000),(101,'Diya Krishna',13000),(102,'Deepa Krishna',14000);")
+#read data(select query)
+cur.execute("select * from employeeinfo")
+
 emp=cur.fetchall()
 
 for i in emp:
     print(i)
 
+conn.commit()'''
+'''#update data(update query)
+cur.execute("update employeeinfo set emp_name='Krishna Kumar' where emp_id=102;")
+conn.commit()'''
+#delete data (delete query)
+cur.execute("delete from employeeinfo where emp_id=100;")
 conn.commit()
 cur.close()
 conn.close()
+
 
 
 
